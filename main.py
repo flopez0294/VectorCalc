24c19a2a6eb84f222c645d930934ea04a7bbbb5f
Jonathan Asai <jacksonm4a3@gmail.com>
2024-11-03 11:07:14 -0800

    Add files via upload
    Added a simple on/off cursor when the thumb is extended on the right hand.

--- main.py
+++ main.py
@@ -42,21 +42,31 @@ class handDetector():
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 lmlist.append([id, cx, cy])
                 if draw:
-                    cv2.circle(img, (cx, cy), 3, (255, 0, 255), cv2.FILLED)
+                    #cv2.circle(img, (cx, cy), 3, (255, 0, 255), cv2.FILLED)
+                    cv2.putText(img,str(int(id)),(cx,cy),cv2.FONT_HERSHEY_PLAIN,1,(255,0,255),1)
         return lmlist
 
 
 # Draws the axis for vector graphing    
 def axis(img):
     thickness = 3
-    cv2.line(img, (img.shape[1]-DIM, 0), (img.shape[1]-DIM, DIM), (0, 0, 255), thickness)
-    cv2.line(img, (img.shape[1]-DIM, DIM), (img.shape[1], DIM), (0, 0, 255), thickness)
-    cv2.line(img, (img.shape[1]-(DIM*2), 0), (img.shape[1]-(DIM*2), DIM), (0, 0, 255), thickness)
-    cv2.line(img, (img.shape[1]-(DIM*2), DIM), (img.shape[1]-DIM, DIM), (0, 0, 255), thickness)
-    cv2.line(img, (img.shape[1]-(DIM*2), DIM), (img.shape[1]-(DIM*2), (DIM*2)), (0, 0, 255), thickness)
-    cv2.line(img, (img.shape[1]-(DIM*2), (DIM*2)), (img.shape[1]-DIM, (DIM*2)), (0, 0, 255), thickness)
-    cv2.line(img, (img.shape[1]-DIM, DIM), (img.shape[1]-DIM, (DIM*2)), (0, 0, 255), thickness)
-    cv2.line(img, (img.shape[1]-DIM, (DIM*2)), (img.shape[1], (DIM*2)), (0, 0, 255), thickness)
+    color = (0, 0, 255)
+    cv2.line(img, (img.shape[1]-DIM, 0), (img.shape[1]-DIM, DIM), color, thickness)
+    cv2.line(img, (img.shape[1]-DIM, DIM), (img.shape[1], DIM), color, thickness)
+    cv2.line(img, (img.shape[1]-(DIM*2), 0), (img.shape[1]-(DIM*2), DIM), color, thickness)
+    cv2.line(img, (img.shape[1]-(DIM*2), DIM), (img.shape[1]-DIM, DIM), color, thickness)
+    cv2.line(img, (img.shape[1]-(DIM*2), DIM), (img.shape[1]-(DIM*2), (DIM*2)), color, thickness)
+    cv2.line(img, (img.shape[1]-(DIM*2), (DIM*2)), (img.shape[1]-DIM, (DIM*2)), color, thickness)
+    cv2.line(img, (img.shape[1]-DIM, DIM), (img.shape[1]-DIM, (DIM*2)), color, thickness)
+    cv2.line(img, (img.shape[1]-DIM, (DIM*2)), (img.shape[1], (DIM*2)), color, thickness)
+    return img
+
+def cursor(img,points):
+    thickness = 1
+    offset = 15
+    color = (0, 255, 0)
+    cv2.line(img, (points[1]-offset, points[2]), (points[1]+offset,points[2]), color, thickness)
+    cv2.line(img, (points[1], points[2]-offset), (points[1], points[2]+offset), color, thickness)
     return img
 
 def main():
@@ -77,7 +87,6 @@ def main():
     # vectors is x distance from origin, y distance from origin, angle made
     success, img = cap.read()
     vecs = np.array([img.shape[1]-DIM,DIM,0], dtype=float)
-    
     while True:
         #get
         success, img = cap.read()
@@ -87,8 +96,16 @@ def main():
         img = axis(img)
         img = detector.findHands(img)
         lmlist = detector.findPosition(img)
+        
+        #Gesture detection
         if len(lmlist) >= 8:
-            print(lmlist[8])
+            #Detects if the thumb is in or out
+            if lmlist[4][1]<=lmlist[3][1]:
+                # Activates cursor if the thumb is out
+                img = cursor(img,lmlist[8])
+
+        #if len(lmlist) >= 8:
+        #    print(lmlist[8])
         
         # calculate the frames
         cTime = time.time()
@@ -103,6 +120,7 @@ def main():
         cv2.putText(img, "Settings", (10, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
         cv2.putText(img, str(int(fps)),(10, img.shape[0] - 30), cv2.FONT_HERSHEY_PLAIN, 3, (255,0, 255),3)
         cv2.imshow("Image", img)
+
         # When you press q it quits the program
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
